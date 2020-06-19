6d2cce93d517ee049b8ac37d7d4eae8e067441d8
sivaramakrishnan1998 <sivaposhac1998@gmail.com>
2020-06-19 17:58:11 +0530

    update main_func

--- Thread/main_func.cpp
+++ Thread/main_func.cpp
@@ -2,21 +2,32 @@
 
 static vector<thread> abc;
 
+std::vector<Stop_while> test_space_list; 
+
+
+
+void initialize(int value){
+    for(auto index = 0; index < value; index++){
+            Stop_while slot;
+            test_space_list.emplace_back(slot);
+    }
+}
 
 Stop_while s;
 
 void thread_run(){
-    thread t1 = thread(&Stop_while::helloFunction, &s);
+    thread t1 = thread(&Stop_while::helloFunction, &test_space_list[1]);
     abc.push_back(std::move(t1));
 }
 
 void thread_break(){
-    thread t2 = thread(&Stop_while::change_val, &s);
+    thread t2 = thread(&Stop_while::change_val, &test_space_list[1]);
     abc.push_back(std::move(t2));
 }
 
 int main(){
-    
+    initialize(8);
+
     thread_run();
     thread_break();
 
